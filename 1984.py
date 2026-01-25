9307af7cc8d22aaf4bf96d8ceebcd2dd2df0b3f5
Stevan Zhuang <stevan.zhuang@gmail.com>
2026-01-24 22:17:39 -0800

    daily

---
+++ 1984.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        min_diff = (10**5)+1
+        for i in range(len(nums) - k + 1):
+            min_diff = min(min_diff, nums[i+k-1] - nums[i])
+        return min_diff
